1ab685a5b46c80cc4380a8408a9637c37209fa9e
RedEmber-hub <redember38@gmail.com>
2025-09-15 16:10:17 +0300

    [TASK-0003] добавила функцию для преобразования имени в инициалы. Эта ката принимает строго два слова с одним пробелом между ними.

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function calculatePoints(matches) {
- return matches.reduce((points, match) => {
-    const [x, y] = match.split(":").map(Number);
-    
-    if (x > y) return points + 3;      
-    if (x === y) return points + 1;    
-    return points;                       
- }, 0)
+function getInitials(string) {
+    return string.split(" ").map((word) => word[0].toUpperCase()).join(".");
 }
 
-console.log(calculatePoints(["3:1", "2:2", "0:1"]));
+console.log(getInitials("Dinara Savrandeeva"));
