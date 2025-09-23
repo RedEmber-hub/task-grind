a0c8493723f2ed2bf88867777cb16802bdaefec6
RedEmber-hub <redember38@gmail.com>
2025-09-23 16:41:22 +0300

    заменила let на const, поменяла название переменной

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function classAverage(points, number) {
-    let sum = [...points, number];
-    return number > sum.reduce((a, b) => a + b) / sum.length
+    const rating = [...points, number];
+
+    return number > rating.reduce((a, b) => a + b) / rating.length
 }
 
 console.log(classAverage([48, 56, 62, 65, 70, 76, 78, 80, 86], 72));
