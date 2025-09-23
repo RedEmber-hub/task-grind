b2d14a80a064185e8b2173b5d4cdfff3f5a8fd28
RedEmber-hub <redember38@gmail.com>
2025-09-23 16:40:06 +0300

    убрала замыкание

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function classAverage(array) {
-    return function(number) {
-        return number > [...array, number].reduce((a, b) => a + b) / [...array, number].length
-    }
+function classAverage(points, number) {
+    let sum = [...points, number];
+    return number > sum.reduce((a, b) => a + b) / sum.length
 }
 
-console.log(classAverage([48, 56, 62, 65, 70, 76, 78, 80, 86]) (72));
+console.log(classAverage([48, 56, 62, 65, 70, 76, 78, 80, 86], 72));
